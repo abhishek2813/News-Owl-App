39aa93f6722b384d222de8caa578e740a57b4e24
abhishek2813 <abhi93158879@gmail.com>
2023-03-22 22:14:58 +0530

    Updates

---
+++ precache-manifest.11565b356a2dc0306de1a20e7f0ebbaa.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "0731f6ed8f054b95d92e",
+    "url": "/News-Owl-App/static/css/main.bb094020.chunk.css"
+  },
+  {
+    "revision": "0731f6ed8f054b95d92e",
+    "url": "/News-Owl-App/static/js/main.0731f6ed.chunk.js"
+  },
+  {
+    "revision": "a89266deb73d87b01859",
+    "url": "/News-Owl-App/static/js/1.a89266de.chunk.js"
+  },
+  {
+    "revision": "aef28c8ee8549b5a2e51",
+    "url": "/News-Owl-App/static/js/2.aef28c8e.chunk.js"
+  },
+  {
+    "revision": "8d450e9c78773220748b",
+    "url": "/News-Owl-App/static/js/runtime~main.8d450e9c.js"
+  },
+  {
+    "revision": "e19e93c6f079af7c7e21c93f94285b36",
+    "url": "/News-Owl-App/static/media/loading.e19e93c6.gif"
+  },
+  {
+    "revision": "773cddd92180222d1535341b29554e4a",
+    "url": "/News-Owl-App/index.html"
+  }
+];
